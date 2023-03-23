b2cb2a54304879545302c615bd69f1e580acc6d0
Tang yicong <49635772+TANGYICONG@users.noreply.github.com>
2023-03-23 11:29:16 +0800

    增加eslintrc、prettierrc等格式化配置

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# vite+vue-ts+less 模板
+# vite+vue3+ts+less+antD 模板
+
+# node>=18.10.0
 
 ## 自用
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "vite";
-import AutoImport from "unplugin-auto-import/vite";
-import vue from "@vitejs/plugin-vue";
-import globalData from "@/style/global.less";
+import { defineConfig } from 'vite'
+import AutoImport from 'unplugin-auto-import/vite'
+import vue from '@vitejs/plugin-vue'
+import globalData from '@/style/global.less'
+import ViteComponents, { AntDesignVueResolver } from 'vite-plugin-components'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,27 +10,30 @@ export default defineConfig({
     vue(),
     AutoImport({
       include: [/\.ts$/, /\.vue$/],
-      imports: ["vue", "vue-router"],
-      dts: "./types/auto-imports.d.ts",
+      imports: ['vue', 'vue-router'],
+      dts: './types/auto-imports.d.ts'
     }),
+    ViteComponents({
+      customComponentResolvers: [AntDesignVueResolver()]
+    })
   ],
   css: {
     preprocessorOptions: {
       less: {
         charset: false,
-        additionalData: globalData,
-      },
-    },
+        additionalData: globalData
+      }
+    }
   },
 
   server: {
     proxy: {
-      "/api": "https://pokeapi.co/",
-    },
+      '/api': 'https://pokeapi.co/'
+    }
   },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "src"),
-    },
-  },
-});
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
+})
